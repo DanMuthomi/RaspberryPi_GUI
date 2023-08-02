ea15bcde08d50b473b4bd401df4e9e7f17dbe8e5
DanMuthomi <thedanielmuthomi@gmail.com>
2023-08-02 08:41:40 +0100

    working well without errors@

--- TKinter/Try2/logged_in.py
+++ TKinter/Try2/logged_in.py
@@ -1,26 +1,23 @@
 import tkinter as tk
+import home
 
-def root_frame(parent, prev_callback):
-    def logout():
-        # Replace this function with the logout logic of your application
-        label_prompt.config(text="Logout button clicked")
-        parent.destroy()
-        prev_callback()  # Transition back to the previous window
-
-    frame = tk.Frame(parent, bg='black')
+def root_frame(parent_frame, prev_callback):
+    frame = tk.Frame(parent_frame, bg='black')
 
     label_prompt = tk.Label(frame, text="", font=('Bold', 20), bg='black', fg='white')
     label_prompt.pack(pady=20)
 
-    # First button (Blank button)
+    def logout():
+        label_prompt.config(text="Logout button clicked")
+        frame.destroy()  # Destroy the current frame
+
+        # Call the previous callback function to transition back to the home page
+        prev_callback()
+
     blank_btn = tk.Button(frame, text='Blank Button', font=('Bold', 20), bd=0, command=lambda: None)
     blank_btn.pack(pady=20)
 
-    # Second button (Logout button)
     logout_btn = tk.Button(frame, text='Logout', font=('Bold', 20), bd=0, command=logout)
     logout_btn.pack(pady=20)
 
-    frame.pack_propagate(False)
-    frame.configure(width=800, height=480)
-    
     return frame
